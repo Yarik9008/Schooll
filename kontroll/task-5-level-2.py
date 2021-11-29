d99b8a75f6fe841421decb2f3f5c46a02739db80
Yarik9008 <iaroslav.muhatdinov@yandex.ru>
2021-11-29 10:28:09 +0300

    kontroll

---
+++ kontroll/task-5-level-2.py
@@ -0,0 +1,24 @@
+path1 = 'kontroll/data/КР5-5.txt'
+
+mass = open(path1, 'r').readlines()
+ful = [int(a[:-1]) for a in mass[:-1]]
+ful.append(int(mass[-1]))
+
+#print('input: ', *ful)
+
+min3 = min(list(filter(lambda x: x % 3 == 0, ful)))
+min5 = min(list(filter(lambda x: x % 5 == 0, ful)))
+print('min3', min3)
+print('min5', min5)
+
+for i in range(len(ful)):
+    if ful[i] % 15 == 0:
+        ful[i] -= min3 + min5
+    elif ful[i] % 5 == 0:
+        ful[i] -= min5
+    elif ful[i] % 3 == 0:
+        ful[i] -= min3
+
+print('out: ', *ful)
+
+# правильно
